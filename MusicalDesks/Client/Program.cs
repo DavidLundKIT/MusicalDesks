770ba43ff492220301faa52ea8a444ecfa2f6f80
David Lund <david.lund@knowit.se>
2022-10-15 13:53:38 +0200

    Added BlazorStrap nuget for Bootstrap components in Blazor

--- MusicalDesks/Client/Program.cs
+++ MusicalDesks/Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorStrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MusicalDesks.Client;
@@ -13,7 +14,7 @@ namespace MusicalDesks.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddBlazorStrap();
             await builder.Build().RunAsync();
         }
     }
